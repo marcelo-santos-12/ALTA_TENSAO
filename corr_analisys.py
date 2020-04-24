33664a9b9f5af48f2c2f19b7e7e1f0d783045f78
Marcelo dos Santos <marcelo@debian-marcelo.debian>
2020-04-23 21:49:01 -0300

    Atualizando script de Correlaçao

--- corr_analisys.py
+++ corr_analisys.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+import numpy as np
 
 columns_format = {
     'rede_rodoviaria': ['Rede Roviária', 'RR'],
@@ -19,10 +21,8 @@ def main():
     
     df = pd.read_csv('variaveis_jan_2020_pa.csv')
     df = df.drop(columns=['ano'])
-    print(df.columns)
-    print(columns_format.keys())
-
-    quit()
+    
+    path_out = 'resultados'
 
     cols = []    
     for _, col in  columns_format.values():
@@ -31,24 +31,14 @@ def main():
     df.columns = cols
     corr = df.corr()
     
-    
-    '''
-    plt.matshow(corr, cmap='Spectral', interpolation='none')  
-    plt.colorbar()
-    plt.xticks(range(len(corr)), cols)
-    plt.yticks(range(len(corr)), cols)
-    plt.show()
-    '''
-    import seaborn as sns
-    import numpy as np
-
     f, ax = plt.subplots(figsize=(9, 6))
     
     mask = np.zeros_like(corr, dtype=np.bool)
     mask[np.triu_indices_from(mask, 1)] = True
     sns.heatmap(corr, mask=mask, cmap='RdBu', annot=True, linewidths=.5, ax=ax)
-    plt.savefig("correl.png")  
+    plt.savefig(path_out + '/' + "correl.png")  
     plt.show()
+
 
 if __name__ == '__main__':
 
